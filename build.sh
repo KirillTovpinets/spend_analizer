a4ccdf0708788d4b1f351c5dccb6eeb1c96972fc
toupinetswillo <ktoupinets@follett.com>
2025-02-11 21:12:49 -0600

    update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update the package list
-sudo apt-get update
+apt-get update
 
 # Install Tesseract OCR
-sudo apt-get install -y tesseract-ocr
+apt-get install -y tesseract-ocr
 
 # Install additional language packs for Tesseract if necessary (optional)
 # sudo apt-get install -y tesseract-ocr-eng tesseract-ocr-other-language
