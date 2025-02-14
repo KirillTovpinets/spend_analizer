2bad204d4d16d113d8e77e1488e3701e994a19ab
toupinetswillo <ktoupinets@follett.com>
2025-02-14 16:15:49 -0600

    update debug setting

--- spend_analizer/settings.py
+++ spend_analizer/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!)r_&-%n*v$q2e+@9yk)$u-av^6yxm!4!t98%g3)x1ceybyat('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['*']
 
